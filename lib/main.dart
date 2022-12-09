7a7172378359c4b3f90bbd2748bc12adf0d9443a
benjamin <benjamin.biollaz@students.hevs.ch>
2022-12-09 10:14:25 +0100

    login

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,8 @@ import 'package:grp_6_bicycle/DTO/RouteDTO.dart';
 import 'package:grp_6_bicycle/all_routes.dart';
 import 'firebase_options.dart';
 
+import 'package:firebase_auth/firebase_auth.dart';
+
 //LINK TO API MAP => `https://wmts.geo.admin.ch/1.0.0/ch.swisstopo.pixelkarte-farbe/default/current/3857/{z}/{x}/{y}.jpeg`
 // carte the base Flutter => https://tile.openstreetmap.org/{z}/{x}/{y}.png
 // https://ch.swisstopo.swisstlm3d-strassen/{z}/{x}/{y}.png
@@ -20,9 +22,30 @@ void main() async {
 }
 
 void readDB() async {
-  RouteDB routeDB = RouteDB();
-  RouteDTO? route = await routeDB.getRouteByName("Cycleway favorite");
-  debugPrint("Height up diff: " + route!.heightDiffUpMeters.toString());
+  //sign up
+  /*
+  try {
+    UserCredential userCredential = await FirebaseAuth.instance
+        .createUserWithEmailAndPassword(
+            email: "alex.moos@mail.com", password: "123456!");
+  } on FirebaseAuthException catch (e) {
+    if (e.code == 'weak-password') {
+      print('The password provided is too weak.');
+    } else if (e.code == 'email-already-in-use') {
+      print('The account already exists for that email.');
+    }
+  } catch (e) {
+    print(e);
+  }*/
+
+  //listen to changes
+  FirebaseAuth.instance.authStateChanges().listen((User? user) {
+    if (user == null) {
+      debugPrint('User is currently signed out!');
+    } else {
+      debugPrint('User is signed in!');
+    }
+  });
 }
 
 class MyApp extends StatelessWidget {
