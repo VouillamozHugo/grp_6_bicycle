45da786baa6230514594d80407a17108e60fade8
benjamin <benjamin.biollaz@students.hevs.ch>
2022-12-16 11:36:34 +0100

    user is redirected to login page when login state changes

--- lib/main.dart
+++ lib/main.dart
@@ -21,10 +21,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: appTitle,
-      home: const LoginWrapper(),
-    ); // MaterialApp
+    return const LoginWrapper(); // MaterialApp
   }
 }
