741e1cbb8e68bb641a1d7d72e899500e2e4f7456
benjamin <benjamin.biollaz@students.hevs.ch>
2023-01-06 13:28:28 +0100

    readonly / edit view

--- lib/Map/details_route.dart
+++ lib/Map/details_route.dart
@@ -29,8 +29,9 @@ class DetailsRoutes extends StatelessWidget {
 
 class DetailsBuilder extends StatefulWidget {
   final RouteDTO route;
-  final bool isRouteEditable;
-  const DetailsBuilder(this.route, this.isRouteEditable, {super.key});
+  bool isAnAdminConnected;
+  // late bool isRouteEditable = isAnAdminConnected;
+  DetailsBuilder(this.route, this.isAnAdminConnected, {super.key});
 
   @override
   State<DetailsBuilder> createState() => _DetailsBuilderState();
@@ -38,6 +39,7 @@ class DetailsBuilder extends StatefulWidget {
 
 class _DetailsBuilderState extends State<DetailsBuilder> {
   late RouteDTO route; //late as parent attribute can't be used as initializer
+  bool isEditActivated = false;
   int buildCount = 0;
   @override
   Widget build(BuildContext context) {
@@ -63,13 +65,14 @@ class _DetailsBuilderState extends State<DetailsBuilder> {
     // as it has a different size and a different layout context
     final routeNameField = setRouteNameField(routeNameTextController);
 
-    // conditional rendering: only admin can update and delete
+    // conditional rendering: only admin can see update, edit, and delete buttons
     final updateRouteButton = setUpdateButton(
         formButtonStyle,
         routeNameTextController,
         startPointTextController,
         endPointTextController);
     final deleteRouteButton = setDeleteButton(formButtonStyle, route.routeName);
+    final switchEditModeButton = setEditButton(formButtonStyle);
 
     return Column(
       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
@@ -77,6 +80,8 @@ class _DetailsBuilderState extends State<DetailsBuilder> {
         Row(
           mainAxisAlignment: MainAxisAlignment.end,
           children: [
+            switchEditModeButton,
+            const SizedBox(width: 10),
             updateRouteButton,
             const SizedBox(width: 10),
             deleteRouteButton,
@@ -85,7 +90,7 @@ class _DetailsBuilderState extends State<DetailsBuilder> {
         ),
         routeNameField,
         SmallMap(startPoint, endPoint, 300, 800),
-        DetailsText(route, widget.isRouteEditable, startPointTextController,
+        DetailsText(route, isEditActivated, startPointTextController,
             endPointTextController),
         GestureDetector(
           behavior: HitTestBehavior.opaque,
@@ -111,12 +116,29 @@ class _DetailsBuilderState extends State<DetailsBuilder> {
   }
 
   Widget setDeleteButton(ButtonStyle buttonStyle, String routeName) {
-    return widget.isRouteEditable
+    //only admin can see the delete button
+    return widget.isAnAdminConnected
         ? OutlinedButton(
             onPressed: () => deleteRoute(routeName),
             style: buttonStyle,
             child: textCreator('Delete', ApplicationConstants.ORANGE))
         : const Center();
+  }
+
+  Widget setEditButton(ButtonStyle buttonStyle) {
+    //only admin can see this button
+    if (!widget.isAnAdminConnected) return const Center();
+
+    //rely on boolean flag to switch from edit to readonly
+    return isEditActivated
+        ? OutlinedButton(
+            onPressed: () => toggleEditMode(),
+            style: buttonStyle,
+            child: textCreator('Readonly mode', ApplicationConstants.ORANGE))
+        : OutlinedButton(
+            onPressed: () => toggleEditMode(),
+            style: buttonStyle,
+            child: textCreator('Edit mode', ApplicationConstants.ORANGE));
   }
 
   Widget setUpdateButton(
@@ -125,13 +147,21 @@ class _DetailsBuilderState extends State<DetailsBuilder> {
     TextEditingController startPointTextController,
     TextEditingController endPointTextController,
   ) {
-    return widget.isRouteEditable
+    //only admin can see the update button
+    return widget.isAnAdminConnected
         ? OutlinedButton(
             onPressed: () => updateRoute(routeNameTextController,
                 startPointTextController, endPointTextController),
             style: buttonStyle,
-            child: textCreator('Edit', ApplicationConstants.ORANGE))
+            child: textCreator('Update route', ApplicationConstants.ORANGE))
         : const Center();
+  }
+
+  //switch from readonly to edit
+  void toggleEditMode() {
+    setState(() {
+      isEditActivated = !isEditActivated; //admin can switch between modes
+    });
   }
 
   void deleteRoute(String routeName) async {
@@ -155,6 +185,8 @@ class _DetailsBuilderState extends State<DetailsBuilder> {
     if (success) {
       setState(() {
         route = newRoute;
+        //display in readonly mode for ux reasons
+        isEditActivated = false;
       });
       return;
     }
@@ -181,7 +213,7 @@ class _DetailsBuilderState extends State<DetailsBuilder> {
       fontSize: 30,
       fontWeight: FontWeight.bold,
     );
-    return widget.isRouteEditable
+    return isEditActivated
         ? TextField(
             style: routeNameTextStyle,
             controller: routeNameTextController,
